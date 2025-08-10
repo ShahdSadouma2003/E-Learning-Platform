daaf70a8a806f3496046cafd759c81af3d61a5cb
ShahdSadouma2003 <148314342+ShahdSadouma2003@users.noreply.github.com>
2025-08-10 11:39:36 +0300

    modifying

--- E Learning/js/carousel.js
+++ E Learning/js/carousel.js
@@ -2,7 +2,7 @@
 
 let slideIndex = 1;
 let timer = null;
-const AUTOPLAY_MS = 5000;
+const AUTOPLAY_MS = 15000;
 
 const slides = () => Array.from(document.querySelectorAll('.mySlides'));
 const dots   = () => Array.from(document.querySelectorAll('.dot'));
